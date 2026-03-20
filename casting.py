2949e65456c11a8f984c42198664719f9fc7b9cd
Bauti <89355938+BautiCev@users.noreply.github.com>
2026-03-20 12:24:25 -0300

    Update casting.py

--- casting.py
+++ casting.py
@@ -3,7 +3,7 @@ def casting():
     #Precio = int(input("Precio: "))
     #Descuento = float(input("Descuento: "))
     precio_descuento = Precio - Descuento
-    cantidad = int(input("Cantidad: "))
+    #cantidad = int(input("Cantidad: "))
     print("Precio: " + str(Precio))
     print("Descuento: " + str(Descuento))
     print("Precio con descuento: " + str(precio_descuento))
